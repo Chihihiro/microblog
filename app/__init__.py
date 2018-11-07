4748efa4d3cd46880ffae237b2e8dbb13eadb4c8
Chihiro <632207812@qq.com>
2018-11-07 16:35:28 +0800

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+
+
+from flask import Flask
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+from app import views
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+from app import views, models
